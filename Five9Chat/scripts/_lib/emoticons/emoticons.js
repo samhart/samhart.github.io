4b4c21dd54f44162f3f0f6faac2504d411ac92f2
Sam Hart <schart@gmail.com>
2015-09-08 14:09:21 -0600

    Added support files

---
+++ Five9Chat/scripts/_lib/emoticons/emoticons.js
@@ -0,0 +1,212 @@
+var DEFINITION = {	
+		"angel":{title:"Angel",codes:["(angel)"], src:"../Five9Chat/images/emoticons/angel.png"},
+		"angry":{title:"Angry",codes:[":@",":-@",":=@","x(","x-(","x=(","X(","X-(","X=("], src:"../Five9Chat/images/emoticons/angry.png"},
+		"bandit":{title:"Bandit",codes:["(bandit)"], src:"../Five9Chat/images/emoticons/bandit.png"},
+		"bear":{title:"Bear",codes:["(bear)","(hug)"], src:"../Five9Chat/images/emoticons/bear.png"},
+		"beer":{title:"Beer",codes:["(beer)"], src:"../Five9Chat/images/emoticons/beer.png"},
+		"big-smile":{title:"Big Smile",codes:[":D",":=D",":-D",":d",":=d",":-d"], src:"../Five9Chat/images/emoticons/bigsmile.png"},
+		"blush":{title:"Blush",codes:["(blush)",":$",":-$",":=$",':">'], src:"../Five9Chat/images/emoticons/blush.png"},
+		"bow":{title:"Bow",codes:["(bow)"], src:"../Five9Chat/images/emoticons/bow.png"},
+		"broken-heart":{title:"Broken heart",codes:["(u)","(U)"], src:"../Five9Chat/images/emoticons/brokenheart.png"},
+		"bug":{title:"Bug",codes:["(bug)"], src:"../Five9Chat/images/emoticons/bug.png"},
+		"cake":{title:"Cake",codes:["(^)","(cake)"], src:"../Five9Chat/images/emoticons/cake.png"},
+		"call":{title:"Call",codes:["(call)"], src:"../Five9Chat/images/emoticons/call.png"},
+		"cash":{title:"Cash",codes:["(cash)","(mo)","($)"], src:"../Five9Chat/images/emoticons/cash.png"},
+		"clapping-hands":{title:"Clapping Hands",codes:["(clap)"], src:"../Five9Chat/images/emoticons/clapping.png"},
+		"coffee":{title:"Coffee",codes:["(coffee)"], src:"../Five9Chat/images/emoticons/coffee.png"},
+		"cool":{title:"Cool",codes:["8)","8=)","8-)","B)","B=)","B-)","(cool)"], src:"../Five9Chat/images/emoticons/cool.png"},
+		"covered-laugh":{title:"Covered Laugh",codes:["(giggle)","(chuckle)"], src:"../Five9Chat/images/emoticons/giggle.png"},
+		"crying":{title:"Crying",codes:[";(",";-(",";=("], src:"../Five9Chat/images/emoticons/crying.png"},
+		"dance":{title:"Dance",codes:["(dance)","\\o/","\\:D/","\\:d/"], src:"../Five9Chat/images/emoticons/dance.png"},
+		"devil":{title:"Devil",codes:["(devil)"], src:"../Five9Chat/images/emoticons/devil.png"},
+		"doh!":{title:"Doh!",codes:["(doh)"], src:"../Five9Chat/images/emoticons/doh.png"},
+		"drink":{title:"Drink",codes:["(d)","(D)"], src:"../Five9Chat/images/emoticons/drink.png"},
+		"drunk":{title:"Drunk",codes:["(drunk)"], src:"../Five9Chat/images/emoticons/drunk.png"},
+		"dull":{title:"Dull",codes:["|(","|-(","|=("], src:"../Five9Chat/images/emoticons/dull.png"},
+		"emo":{title:"Emo",codes:["(emo)"], src:"../Five9Chat/images/emoticons/emo.png"},
+		"envy":{title:"Envy",codes:["(envy)"], src:"../Five9Chat/images/emoticons/envy.png"},
+		"evil-grin":{title:"Evil grin",codes:["]:)",">:)","(grin)"], src:"../Five9Chat/images/emoticons/evilgrin.png"},
+		"face-palm":{title:"Face Palm",codes:["((facepalm))"], src:"../Five9Chat/images/emoticons/facepalm.png"},
+		"fingers-crossed":{title:"Fingers Crossed",codes:["((fingerscrossed))"], src:"../Five9Chat/images/emoticons/fingerscrossed.png"},
+		"finger":{title:"Finger",codes:["(finger)"], src:"../Five9Chat/images/emoticons/middlefinger.png"},
+		"flower":{title:"Flower",codes:["(f)","(F)"], src:"../Five9Chat/images/emoticons/flower.png"},
+		"fubar":{title:"Fubar",codes:["(fubar)"], src:"../Five9Chat/images/emoticons/fubar.png"},
+		"handshake":{title:"Shaking Hands",codes:["(handshake)"], src:"../Five9Chat/images/emoticons/handshake.png"},
+		"happy":{title:"Happy",codes:["(happy)"], src:"../Five9Chat/images/emoticons/happy.png"},
+		"headbang":{title:"Headbang",codes:["(headbang)","(banghead)"], src:"../Five9Chat/images/emoticons/headbang.png"},
+		"heart":{title:"Heart",codes:["(h)","<3","(H)","(l)","(L)"], src:"../Five9Chat/images/emoticons/heart.png"},
+		"heidy":{title:"Heidy",codes:["(heidy)"], src:"../Five9Chat/images/emoticons/heidy.png"},
+		"hi":{title:"Hi",codes:["(hi)"], src:"../Five9Chat/images/emoticons/hi.png"},
+		"high-five":{title:"High Five",codes:["(highfive)"], src:"../Five9Chat/images/emoticons/highfive.png"},
+		"in-love":{title:"In love",codes:["(inlove)"], src:"../Five9Chat/images/emoticons/inlove.png"},
+		"it-wasnt-me":{title:"It wasn't me",codes:["(wasntme)"], src:"../Five9Chat/images/emoticons/itwasntme.png"},
+		"kiss":{title:"Kiss",codes:[":*",":=*",":-*"], src:"../Five9Chat/images/emoticons/kiss.png"},
+		"lalala":{title:"Lalala",codes:["(lalala)"], src:"../Five9Chat/images/emoticons/lalala.png"},
+		"lips-sealed":{title:"Lips Sealed",codes:[":x",":-x",":X",":-X",":#",":-#",":=x",":=X",":=#"], src:"../Five9Chat/images/emoticons/lipssealed.png"},
+		"mail":{title:"Mail",codes:["(e)","(m)"], src:"../Five9Chat/images/emoticons/mail.png"},
+		"make-up":{title:"Make-up",codes:["(makeup)","(kate)"], src:"../Five9Chat/images/emoticons/makeup.png"},
+		"mmm":{title:"Mmm...",codes:["(mm)"], src:"../Five9Chat/images/emoticons/mmm.png"},
+		"mooning":{title:"Mooning",codes:["(mooning)"], src:"../Five9Chat/images/emoticons/mooning.png"},
+		"movie":{title:"Movie",codes:["(~)","(film)","(movie)"], src:"../Five9Chat/images/emoticons/movie.png"},
+		"muscle":{title:"Muscle",codes:["(muscle)","(flex)"], src:"../Five9Chat/images/emoticons/muscle.png"},
+		"music":{title:"Music",codes:["(music)"], src:"../Five9Chat/images/emoticons/music.png"},
+		"nerd":{title:"Nerd",codes:["8-|","B-|","8|","B|","8=|","B=|","(nerd)"], src:"../Five9Chat/images/emoticons/nerd.png"},
+		"ninja":{title:"Ninja",codes:["(ninja)"], src:"../Five9Chat/images/emoticons/ninja.png"},
+		"no":{title:"No",codes:["(n)","(N)"], src:"../Five9Chat/images/emoticons/no.png"},
+		"nodding":{title:"Nodding",codes:["(nod)"], src:"../Five9Chat/images/emoticons/nod.png"},
+		"party":{title:"Party!!!",codes:["(party)"], src:"../Five9Chat/images/emoticons/party.png"},
+		"phone":{title:"Phone",codes:["(mp)","(ph)"], src:"../Five9Chat/images/emoticons/phone.png"},
+		"pizza":{title:"Pizza",codes:["(pizza)","(pi)"], src:"../Five9Chat/images/emoticons/pizza.png"},
+		"poolparty":{title:"Poolparty",codes:["(poolparty)"], src:"../Five9Chat/images/emoticons/poolparty.png"},
+		"puke":{title:"Puke",codes:["(puke)",":&",":-&",":=&"], src:"../Five9Chat/images/emoticons/puke.png"},
+		"punch":{title:"Punch",codes:["(punch)"], src:"../Five9Chat/images/emoticons/punch.png"},
+		"rain":{title:"Rain",codes:["(rain)","(london)","(st)"], src:"../Five9Chat/images/emoticons/rain.png"},
+		"rock":{title:"Rock",codes:["(rock)"], src:"../Five9Chat/images/emoticons/rock.png"},
+		"rofl":{title:"Rolling on the floor laughing",codes:["(rofl)"], src:"../Five9Chat/images/emoticons/rofl.png"},
+		"sad-smile":{title:"Sad Smile",codes:[":(",":=(",":-("], src:"../Five9Chat/images/emoticons/sadsmile.png"},
+		"shaking":{title:"Shaking",codes:["(shake)"], src:"../Five9Chat/images/emoticons/shake.png"},
+		"sleepy":{title:"Sleepy",codes:["|-)","I-)","I=)","(snooze)"], src:"../Five9Chat/images/emoticons/sleepy.png"},
+		"smile":{title:"Smile",codes:[":)",":=)",":-)"], src:"../Five9Chat/images/emoticons/smile.png"},
+		"smirking":{title:"Smirking",codes:["(smirk)"], src:"../Five9Chat/images/emoticons/smirk.png"},
+		"smoking":{title:"Smoking",codes:["(smoking)","(smoke)","(ci)"], src:"../Five9Chat/images/emoticons/smoke.png"},
+		"speechless":{title:"Speechless",codes:[":|",":=|",":-|"], src:"../Five9Chat/images/emoticons/speechless.png"},
+		"star":{title:"Star",codes:["(*)"], src:"../Five9Chat/images/emoticons/star.png"},
+		"sun":{title:"Sun",codes:["(sun)"], src:"../Five9Chat/images/emoticons/sun.png"},
+		"swearing":{title:"Swearing",codes:["(swear)"], src:"../Five9Chat/images/emoticons/swear.png"},
+		"sweating":{title:"Sweating",codes:["(sweat)","(:|"], src:"../Five9Chat/images/emoticons/sweating.png"},
+		"talking":{title:"Talking",codes:["(talk)"], src:"../Five9Chat/images/emoticons/talking.png"},
+		"thinking":{title:"Thinking",codes:["(think)",":?",":-?",":=?"], src:"../Five9Chat/images/emoticons/thinking.png"},
+		"time":{title:"Time",codes:["(o)","(O)","(time)"], src:"../Five9Chat/images/emoticons/time.png"},
+		"tmi":{title:"TMI",codes:["(tmi)"], src:"../Five9Chat/images/emoticons/tmi.png"},
+		"toivo":{title:"Toivo",codes:["(toivo)"], src:"../Five9Chat/images/emoticons/toivo.png"},
+		"tongue-out":{title:"Tongue Out",codes:[":P",":=P",":-P",":p",":=p",":-p"], src:"../Five9Chat/images/emoticons/tongueout.png"},
+		"tumbleweed":{title:"Tumbleweed",codes:["(tumbleweed)"], src:"../Five9Chat/images/emoticons/tumbleweed.png"},
+		"wait":{title:"Wait",codes:["(wait)"], src:"../Five9Chat/images/emoticons/wait.png"},
+		"waiting":{title:"Waiting",codes:["(waiting)"], src:"../Five9Chat/images/emoticons/waiting.png"},
+		"whew":{title:"Whew",codes:["(whew)"], src:"../Five9Chat/images/emoticons/whew.png"},
+		"wfh":{title:"Working from home",codes:["(wfh)"], src:"../Five9Chat/images/emoticons/wfh.png"},
+		"wink":{title:"Wink",codes:[":o",":=o",":-o",":O",":=O",":-O"], src:"../Five9Chat/images/emoticons/wink.png"},
+		"wondering":{title:"Wondering",codes:[":^)"], src:"../Five9Chat/images/emoticons/wondering.png"},
+		"worried":{title:"Worried",codes:[":S",":-S",":=S",":s",":-s",":=s"], src:"../Five9Chat/images/emoticons/worried.png"},
+		"yawn":{title:"Yawn",codes:["(yawn)","|-()"], src:"../Five9Chat/images/emoticons/yawn.png"},
+		"yes":{title:"Yes",codes:["(y)","(Y)","(ok)"], src:"../Five9Chat/images/emoticons/yes.png"}};
+
+
+(function($, exports, window, name) {
+
+if (!exports) {
+    exports = {};
+
+    if ($) {
+        $[name] = exports;
+    } else {
+        window[name] = exports;
+    }
+}
+
+var emoticons,
+    codesMap = {},
+    primaryCodesMap = {},
+    regexp,
+    metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g,
+    entityMap;
+
+entityMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;'
+};
+
+function escape(string) {
+    return String(string).replace(/[&<>"'\/]/g, function(s) {
+        return entityMap[s];
+    });
+}
+
+/**
+ * Define emoticons set.
+ *
+ * @param {Object} data
+ */
+exports.define = function() {
+    var name, i, codes, code,
+        patterns = [];
+    
+    var data =  DEFINITION;
+
+    for (name in data) {
+        codes = data[name].codes;
+        for (i in codes) {
+            code = codes[i];
+            codesMap[code] = name;
+
+            // Create escaped variants, because mostly you want to parse escaped
+            // user text.
+            codesMap[escape(code)] = name;
+            if (i == 0) {
+                primaryCodesMap[code] = name;
+            }
+        }
+    }
+
+    for (code in codesMap) {
+        patterns.push('(' + code.replace(metachars, "\\$&") + ')');
+    }
+
+    regexp = new RegExp(patterns.join('|'), 'g');
+    emoticons = data;
+};
+
+/**
+ * Replace emoticons in text.
+ *
+ * @param {String} text
+ * @param {Function} [fn] optional template builder function.
+ */
+exports.replace = function(text, fn) {
+    return text.replace(regexp, function(code) {
+        var name = codesMap[code];
+        return (fn || exports.tpl)(name, code, emoticons[name].title, emoticons[name].src);
+    });
+};
+
+/**
+ * Get primary emoticons as html string in order to display them later as overview.
+ *
+ * @param {Function} [fn] optional template builder function.
+ * @return {String}
+ */
+exports.toString = function(fn) {
+    var code,
+        str = '',
+        name;
+
+    for (code in primaryCodesMap) {
+        name = primaryCodesMap[code];
+        str += (fn || exports.tpl)(name, code, emoticons[name].title, emoticons[name].src);
+    }
+
+    return str;
+};
+
+/**
+ * Build html string for emoticons.
+ *
+ * @param {String} name
+ * @param {String} code
+ * @param {String} title
+ * @return {String}
+ */
+exports.tpl = function(name, code, title, src) {
+    return "<img class=\"emoticon\" ref=\"" + code + "\" src=\"" +  src+ "\" title=\"" + title + "\"/>";
+    // return "<img class=\"emoticon emoticon-" + name + "\" ref=\"" + code + "\" src=\"" +  src+ "\" title=\"" + title + "\"/>";
+    /* return '<span class="emoticon emoticon-' + name + '" title="' + title + '">' + code + '</span>'; */
+};
+
+}(typeof jQuery != 'undefined' ? jQuery : null,
+  typeof exports != 'undefined' ? exports : null,
+  window,
+  'emoticons'));
---
+++ Five9Chat/scripts/session/Access.js
@@ -0,0 +1,11 @@
+var ACCESS = new Object();
+
+/**
+ * Production
+ */
+ACCESS.TOKEN_URL                = "https://sococare.five9.com";
+ACCESS.ADMIN_URL                = "https://us4.five9.com/scim";
+ACCESS.ADMIN_REPORT_URL         = "https://us4.five9.com/SOCOReports/SCIMAdminReports";
+ACCESS.AGENT_REPORT_URL         = "https://us4.five9.com/SOCOReports/SCIMReports";
+ACCESS.SOCKET_URL               = "wss://sccws4.five9.com/";
+
